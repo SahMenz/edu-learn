c49e856eaeddadc416d1d872ef7b421de5a0da81
Sah Menz <mensahemma670@gmail.com>
2023-04-04 13:45:53 +0100

    Updates

---
+++ precache-manifest.da81647065224ec7e117b5aa6c57649f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7165538fa92fbe159ad",
+    "url": "/SahMenz.github.io/edu-learn/static/css/main.abb3439a.chunk.css"
+  },
+  {
+    "revision": "f7165538fa92fbe159ad",
+    "url": "/SahMenz.github.io/edu-learn/static/js/main.f7165538.chunk.js"
+  },
+  {
+    "revision": "5a5278d1664fc398e049",
+    "url": "/SahMenz.github.io/edu-learn/static/js/1.5a5278d1.chunk.js"
+  },
+  {
+    "revision": "611320c55cb2165170bd",
+    "url": "/SahMenz.github.io/edu-learn/static/js/runtime~main.611320c5.js"
+  },
+  {
+    "revision": "34eb65e831cb52c60f7451262427fac5",
+    "url": "/SahMenz.github.io/edu-learn/static/media/dots.34eb65e8.svg"
+  },
+  {
+    "revision": "a4cf4367e7bd12e31b50232614bca0f8",
+    "url": "/SahMenz.github.io/edu-learn/static/media/greenTicks.a4cf4367.svg"
+  },
+  {
+    "revision": "0d303df13d3ea52f609e52cf374ad6ab",
+    "url": "/SahMenz.github.io/edu-learn/static/media/Star1.0d303df1.svg"
+  },
+  {
+    "revision": "b3da9135f746e608ed05b68b31668869",
+    "url": "/SahMenz.github.io/edu-learn/static/media/book.b3da9135.svg"
+  },
+  {
+    "revision": "c5836e5963a400a7d1c75b3f5846351b",
+    "url": "/SahMenz.github.io/edu-learn/static/media/engineer.c5836e59.svg"
+  },
+  {
+    "revision": "271091c03f89e3ad5e5fd2b4b614c6a3",
+    "url": "/SahMenz.github.io/edu-learn/static/media/technology.271091c0.svg"
+  },
+  {
+    "revision": "21f8e839283c693dada3c713b3f45e8f",
+    "url": "/SahMenz.github.io/edu-learn/static/media/science.21f8e839.svg"
+  },
+  {
+    "revision": "87d8f18c4fad4f7c766013bb4dc61e55",
+    "url": "/SahMenz.github.io/edu-learn/static/media/searchh.87d8f18c.svg"
+  },
+  {
+    "revision": "ae423a00deb66deabac81ea3a83e8e23",
+    "url": "/SahMenz.github.io/edu-learn/static/media/expand.ae423a00.svg"
+  },
+  {
+    "revision": "2634c4cb30d78efcd7061420bbdd03ca",
+    "url": "/SahMenz.github.io/edu-learn/static/media/beautyTwo.2634c4cb.svg"
+  },
+  {
+    "revision": "38ac9524f0406586b49238f5c9076c84",
+    "url": "/SahMenz.github.io/edu-learn/index.html"
+  }
+];
